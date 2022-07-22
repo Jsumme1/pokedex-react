b9b037a8afdfd251eac2fe6de3b647f173093544
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-07-22 12:15:25 -0500

    finished project

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Pokegame from './Pokegame';
 import Pokedex from './Pokedex';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
